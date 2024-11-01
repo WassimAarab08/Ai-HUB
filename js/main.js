f1125bf674e199f0ebb043400989234174812558
EasyStar1 <wassimaarab18@gmail.com>
2024-11-01 21:51:38 +0100

    Implement mobile navigation functionality and styles; add event listeners for dropdown menus and adjust CSS for responsive design

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 const deskLiDrop = document.getElementById('navbarDropdown');
 const iconArrow = document.getElementById('dropdownArrow');
 const dropdownDeks = document.getElementById('dropdownMenu');
-
+const mobileButton = document.getElementById('mobileButton');
+const mobileVersion = document.getElementById('mobileVersion');
+const mobileButtonIcon = document.getElementById('mobileButtonIcon');
+const mobileDropdownMenu = document.getElementById('mobileDropdownMenu');
+const mobileNavbarDropdown = document.getElementById('mobileNavbarDropdown');
+const mobileNavbarList = document.getElementById('mobileNavbarList');
+const mobileDropdownArrow = document.getElementById('mobileDropdownArrow');
 // Escucha el evento de clic en el botón desplegable
 deskLiDrop.addEventListener('click', function (event) {
     event.stopPropagation();
@@ -29,3 +35,39 @@ document.addEventListener('click', function (event) {
     }
 });
 
+
+
+
+
+
+
+
+mobileButton.addEventListener('click', () => {
+    if (mobileVersion.style.display === 'block') {
+        // Ocultar menú
+        mobileVersion.style.display = 'none';
+        mobileButtonIcon.style.transform = 'rotate(0deg)';
+    } else {
+        // Mostrar menú con animación
+        mobileVersion.style.display = 'block';
+        mobileVersion.classList.add('slideDown');
+
+    }
+});
+
+
+
+
+mobileNavbarDropdown.addEventListener('click', () => {
+    if (mobileDropdownMenu.classList.contains('show')) {
+        // Ocultar menú
+        mobileNavbarList.style.height = '23vh';
+        mobileDropdownMenu.classList.remove('show');
+        mobileDropdownArrow.style.transform = 'rotate(0deg)';
+    } else {
+        // Mostrar menú con animación
+        mobileDropdownMenu.classList.add('show');
+        mobileNavbarList.style.height = '35vh';
+        mobileDropdownArrow.style.transform = 'rotate(180deg)'; // Cambia la dirección del ícono si lo deseas
+    }
+});
